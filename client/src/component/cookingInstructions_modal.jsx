f5fe7c915fc63a1f1443b07f3faf77019f5c6dbf
linvvian <lin.vvian@gmail.com>
2017-07-25 13:51:09 -0400

    sets tokens to environment variables

--- client/src/component/cookingInstructions_modal.jsx
+++ client/src/component/cookingInstructions_modal.jsx
@@ -1,13 +1,36 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Button, Header, Icon, Modal, Form, Input, TextArea, Dropdown, Label } from 'semantic-ui-react'
+import { Button, Header, Icon, Modal, Form, Input, Grid } from 'semantic-ui-react'
 
 class ShowInstructionsModal extends Component {
   render(){
     return(
-      <div></div>
+      <Modal trigger={<Button primary className='button_basic2' floated='right'><Icon name='food' /> Cook It</Button>}>
+        <Modal.Header>Steps</Modal.Header>
+        <div id='steps_modal'>
+        <Modal.Content>
+          <Modal.Description>
+            <Grid container centered={true} verticalAlign='middle' column={3}>
+              <Grid.Column width={2}>
+                <Button circular primary className='button_basic' icon='arrow left' floated='left'/>
+              </Grid.Column>
+              <Grid.Column width={12}>
+                <Header><div id='steps_text'>{this.props.instructions[0].step}</div></Header>
+              </Grid.Column>
+              <Grid.Column width={2}>
+                <Button circular primary className='button_basic' icon='arrow right' floated='right'/>
+              </Grid.Column>
+            </Grid>
+          </Modal.Description>
+        </Modal.Content>
+        </div>
+      </Modal>
     )
   }
 }
 
-export default ShowInstructionsModal
+const mapStateToProps = (state) => {
+  return { instructions: state.recipe.instructions }
+}
+
+export default connect(mapStateToProps)(ShowInstructionsModal)
--- server/app/controllers/notifications_controller.rb
+++ server/app/controllers/notifications_controller.rb
@@ -4,17 +4,9 @@ class NotificationsController < ApplicationController
     render json: { message: "Message Sent" }
   end
   private
-  def account_sid
-    'ACd545c43b8144c51c1a2e139d5ab00827'
-  end
-
-  def auth_token
-    '84a49be88138c72740f9f7f4db4b69be'
-  end
-
   def send_message(phone_number, message)
     twilio_number = '+12015286979'
-    @client = Twilio::REST::Client.new account_sid, auth_token
+    @client = Twilio::REST::Client.new twilio_sid, twilio_api_token
     message = @client.messages.create(
       from: twilio_number,
       to: phone_number,
